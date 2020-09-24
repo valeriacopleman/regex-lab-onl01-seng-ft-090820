ced29d38bdc3fbd10500816649a96a4a6c9d67fe
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-24 06:52:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]+\w/)
+      return true
+   else
+     return false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
