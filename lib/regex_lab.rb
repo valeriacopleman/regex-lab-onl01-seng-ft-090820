e5b93230c3608333183bfddcc467af96e0460ff2
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-24 07:27:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(//)
+  if phone.match(/\d{3}/)
     return true
   else
     return false
