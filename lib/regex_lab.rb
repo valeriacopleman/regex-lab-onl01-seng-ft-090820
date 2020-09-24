efa0aecdf559aacca64a3910c4a008d25962d7b2
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-24 07:21:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
   if word.match(/^[aeiouAEIOU]/)
-      return true
-   else
-     return false
-   end
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,20 +15,18 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-   if text.match(/^[A-Z].+[?.!]$/)
-       return true
-    else
-        return false
-    end
+  if text.match(/^[A-Z].+[?.!]$/)
+    return true
+  else
+    return false
   end
+end
 
 def valid_phone_number?(phone)
-    if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
-      then
-       return true
-    else
-       return false
-    end
-    
+  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+    return true
+  else
+    return false
   end
+end
 
